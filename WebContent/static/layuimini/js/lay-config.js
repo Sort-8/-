09130e54f7e18158e99e50af9c90f6d4ed09e35c
阿海 <oncwnuP9j-MqdFla7hxbpaC9K6NU@git.weixin.qq.com>
2021-06-24 11:37:19 +0800

    删除layui

--- WebContent/static/layuimini/js/lay-config.js
+++ WebContent/static/layuimini/js/lay-config.js
@@ -9,7 +9,6 @@ window.rootPath = (function (src) {
     src = document.scripts[document.scripts.length - 1].src;
     return src.substring(0, src.lastIndexOf("/") + 1);
 })();
-
 layui.config({
     base: rootPath + "lay-module/",
     version: true
