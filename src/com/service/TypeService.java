672a92bc21cb5f836565b0e16b7f887e2bb4c91e
阿海 <oncwnuP9j-MqdFla7hxbpaC9K6NU@git.weixin.qq.com>
2021-06-26 09:51:45 +0800

    添加注释

--- src/com/service/TypeService.java
+++ src/com/service/TypeService.java
@@ -6,9 +6,12 @@ import com.entity.Book_type;
 
 public interface TypeService {
 	
+	/*返回错误信息*/
 	public String getErrorMsg();
 	
+	/*搜索图书类型*/
 	public List<Book_type> searchBookType(String type_id);
 
+	/*返回所有图书类型*/
 	public List<Book_type> getAllType();
 }
